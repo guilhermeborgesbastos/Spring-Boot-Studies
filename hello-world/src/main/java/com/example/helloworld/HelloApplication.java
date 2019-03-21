3cff555e2b8809418ddf08fac441c9b812da075d
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2019-03-21 20:47:48 -0300

    First commit

---
+++ hello-world/src/main/java/com/example/helloworld/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+}
+
+
+@RestController
+class Hello {
+
+    @RequestMapping("/")
+    String index() {
+        return "Hello world";
+    }
+}
