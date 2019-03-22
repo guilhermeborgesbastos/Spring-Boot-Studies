08e21acf7017004da1a54d0ad9ee97e793d8ff77
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2019-03-21 21:17:25 -0300

    Spring Boot Servlet Initializer
    - Extended the class SpringBootServletInitializer to support WAR file
    deployment.

--- hello-world/src/main/java/com/example/helloworld/HelloApplication.java
+++ hello-world/src/main/java/com/example/helloworld/HelloApplication.java
@@ -2,18 +2,23 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class HelloApplication {
+public class HelloApplication extends SpringBootServletInitializer{
 
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(HelloApplication.class);
+   }
+   
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
-
 }
-
 
 @RestController
 class Hello {
