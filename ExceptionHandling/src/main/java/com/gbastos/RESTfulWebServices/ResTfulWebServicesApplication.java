2ab50a1fb2515e8647e556c5fea460c53248008d
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2019-04-20 17:52:04 -0300

    Spring Boot - Exception Handling
    - Basic RESTfull API structure;

---
+++ ExceptionHandling/src/main/java/com/gbastos/RESTfulWebServices/ResTfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.gbastos.RESTfulWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulWebServicesApplication.class, args);
+	}
+
+}
