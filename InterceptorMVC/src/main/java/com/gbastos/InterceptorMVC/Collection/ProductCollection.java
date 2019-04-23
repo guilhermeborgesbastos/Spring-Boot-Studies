452731c4fdeaf52e64a40e48fca27989abdd3caf
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2019-04-23 20:33:35 -0300

    Spring Boot - Interceptor MVC
    - Created the basic Product collection object;

---
+++ InterceptorMVC/src/main/java/com/gbastos/InterceptorMVC/Collection/ProductCollection.java
@@ -0,0 +1,16 @@
+package com.gbastos.InterceptorMVC.Collection;
+
+import java.util.List;
+
+public class ProductCollection {
+
+  private List<Long> productsIds;
+
+  public List<Long> getProductsIds() {
+    return productsIds;
+  }
+
+  public void setProductsIds(List<Long> productsIds) {
+    this.productsIds = productsIds;
+  }
+}
