6b478566ba8551a8751b450b7eeed3162c4cb766
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2019-06-01 15:53:32 -0300

    Spring Boot - RESTful Web Services
    - Removed unnecessary imports;

--- RESTfulWebServices/src/main/java/com/gbastos/RESTfulWebServices/controller/ProductController.java
+++ RESTfulWebServices/src/main/java/com/gbastos/RESTfulWebServices/controller/ProductController.java
@@ -1,14 +1,10 @@
 package com.gbastos.RESTfulWebServices.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gbastos.RESTfulWebServices.model.Product;
