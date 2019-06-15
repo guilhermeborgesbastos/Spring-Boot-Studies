6ecf4c2f24fcf16b51474b2c7977511f257f8833
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2019-06-15 16:49:52 -0300

    Spring Boot - File Handling
    - Added pom and maven configs and application properties;

---
+++ FileHandlerWebService/src/main/java/com/gbastos/FileHandlerWebService/RestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.gbastos.FileHandlerWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+}
